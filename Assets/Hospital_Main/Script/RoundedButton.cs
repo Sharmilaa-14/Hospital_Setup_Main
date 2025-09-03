ed496d25af12892f6777eb85e2e112019c14f9ae
abineshsrinivasan <abinesh701032gmail.com>
2025-09-03 12:19:50 +0530

    eye medication setup arrangement

--- Assets/Hospital_Main/Script/RoundedButton.cs
+++ Assets/Hospital_Main/Script/RoundedButton.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+// Example: Change Button border look dynamically
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoundedButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image buttonImage;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Apply rounded sprite
+        buttonImage.sprite = Resources.Load<Sprite>("rounded_sprite");
+        buttonImage.type = Image.Type.Sliced;
     }
 }
